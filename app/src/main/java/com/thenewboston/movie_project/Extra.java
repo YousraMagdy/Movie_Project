5670d8587b741ccecbfdf74434a08efb942316d6
YousraMagdy <yousramagdy2020@gmail.com>
2016-09-27 00:01:32 +0200

    Initial commit

---
+++ app/src/main/java/com/thenewboston/movie_project/Extra.java
@@ -0,0 +1,23 @@
+package com.thenewboston.movie_project;
+
+
+
+
+public class Extra {
+    String OverView;
+    String Title;
+    String image; // drawable reference id
+    String Vote;
+    String Date;
+    String ID;
+    public Extra(String vtitle, String image,String voverview,String vote,String date,String id)
+    {
+        this.OverView = voverview;
+        this.Title = vtitle;
+        this.image = image;
+        this.Vote=vote;
+        this.Date=date;
+        this.ID=id;
+    }
+
+}
